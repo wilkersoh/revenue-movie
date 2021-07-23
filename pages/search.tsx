e8984bfa5caaff33efc704bf3d96b4edc7a3f9ef
wilkersoh <plgu@hotmail.com>
2021-07-23 21:02:54 +0800

    update: typo

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import App from "@/components/layouts/index"
+import React from "react";
+import App from "@/components/layouts/index";
 
 const search: React.FC = () => {
   return (
     <App>
-    <div>
-      i am search page
-    </div>
+      <div>i am search page</div>
     </App>
-  )
-}
+  );
+};
 
-export default search
+export default search;
--- utils/fetcher.js
+++ utils/fetcher.js
@@ -1,3 +1,5 @@
+// fetch("url.com", { qs: { a: 1, b: 2 } });
+
 export default async (url, token) => {
   const res = await fetch(url, {
     method: "GET",
