6d6ec12faa864ff91fec9882dac6f2dc4134a90c
wilkersoh <plgu@hotmail.com>
2021-07-24 23:57:20 +0800

    update: css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import { SWRConfig } from "swr";
-
+import fetcher from "@/utils/fetcher";
 import "@/styles/globals.tailwind.css";
+import "@/styles/index.css"
 
 function MyApp({ Component, pageProps }) {
   return (
     <SWRConfig
       value={{
         refreshInterval: 5000,
-        fetcher: (resource, init) =>
-          fetch(resource, init).then((res) => res.json()),
+        fetcher
       }}>
       <Component {...pageProps} />
     </SWRConfig>
