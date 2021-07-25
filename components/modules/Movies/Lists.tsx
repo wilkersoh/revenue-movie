b3d12a487c408aaabc17b3e483c1d0a8b8d696e1
wilkersoh <plgu@hotmail.com>
2021-07-25 08:33:26 +0800

    server: test image component in movies

--- components/modules/Movies/Lists.tsx
+++ components/modules/Movies/Lists.tsx
@@ -13,22 +13,25 @@ const Lists: React.FC<ListsProps> = ({ ...movies }) => {
   return (
     <ul className='grid grid-cols-2 md:grid-cols-4 gap-4 first:mt-0 mt-4'>
       {movies?.results &&
-        movies.results.map((movie) => (
-          <li key={movie.id}>
-            <Link href='#'>
-              <a>
-                {/* <img src={imageUrl(movie)} className="w-full" alt="" /> */}
-                <Image
-                  src={imageUrl(movie)}
-                  alt={movie.title}
-                  width={680}
-                  height={400}
-                  layout='responsive'
-                />
-              </a>
-            </Link>
-          </li>
-        ))}
+        movies.results.map((movie, i) =>
+        {
+          if(i > 4) return <div></div>;
+          return (
+            <li key={movie.id}>
+              <Link href='#'>
+                <a>
+                  {/* <img src={imageUrl(movie)} className="w-full" alt="" /> */}
+                  <Image
+                    src={imageUrl(movie)}
+                    alt={movie.title}
+                    width={680}
+                    height={400}
+                    layout='responsive'
+                  />
+                </a>
+              </Link>
+            </li>
+          )})}
     </ul>
   );
 };
