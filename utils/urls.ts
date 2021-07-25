cc5c38624bb8d8255a3d52ad687c99b0c229641c
wilkersoh <plgu@hotmail.com>
2021-07-25 08:57:19 +0800

    server: test image component in movies

--- utils/urls.ts
+++ utils/urls.ts
@@ -4,11 +4,15 @@ export const ORIGINAL_IMAGE_URL = "https://image.tmdb.org/t/p/original";
 export const imageUrl = (image) => {
   if (!image) return "/vercel.svg";
 
-  if (image.backdrop_path)
-    return `${ORIGINAL_IMAGE_URL}${image.backdrop_path}?api_key=${process.env.TMDB_MOVIE_KEY}`;
+  if (image.backdrop_path) {
+    const a = `${ORIGINAL_IMAGE_URL}${image.backdrop_path}?api_key=${process.env.TMDB_MOVIE_KEY}`;
 
-  // if (image.poster_path)
-  //   return `${ORIGINAL_IMAGE_URL}${image.poster_path}?api_key=${process.env.TMDB_MOVIE_KEY}`;
+    console.log(`a:  `, a);
+    return a;
+  }
+
+  if (image.poster_path)
+    return `${ORIGINAL_IMAGE_URL}${image.poster_path}?api_key=${process.env.TMDB_MOVIE_KEY}`;
 
   /**
    * fallback img
