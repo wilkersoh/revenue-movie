d425e3c32055a1009bb5bd5343db191ebb1cf7f1
wilkersoh <plgu@hotmail.com>
2021-07-29 20:18:33 +0800

    server: debug image component

--- utils/useTooltip.tsx
+++ utils/useTooltip.tsx
@@ -4,12 +4,7 @@ const useTooltip = () => {
   const node = useRef();
   const [isVisible, setState] = useState(false);
   const handleClick = ({ target }) => {
-    if (node.current.contains(target)) {
-      // inside click
-      return;
-    }
-    // outside click
-    setState(false);
+
   };
 
   return <div></div>;
